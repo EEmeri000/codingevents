aa6aaa782b7b2e032d532a71a9a6dcc700afeb5e
CannibalCrab <138933689+EEmeri000@users.noreply.github.com>
2023-09-19 11:19:01 -0400

    Modified to Follow along with Chap14, end of Chapter

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -2,11 +2,8 @@ package org.launchcode.codingevents.controllers;
 import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -36,9 +33,9 @@ public class EventController {
     }
 
     @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String eventName, @RequestParam String eventDescription) {
+    public String processCreateEventForm(@ModelAttribute Event newEvent) {
         //events.add(eventName);
-        EventData.add(new Event(eventName, eventDescription));
+        EventData.add(newEvent);
         return "redirect:/events";
     }
     @GetMapping("delete")
