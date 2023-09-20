a76e6e26d225c27b566e912ad33e75416cf109ec
CannibalCrab <138933689+EEmeri000@users.noreply.github.com>
2023-09-20 10:13:12 -0400

    Chap 15 code along Complete

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,20 +1,32 @@
 package org.launchcode.codingevents.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import java.util.Objects;
 
 public class Event {
+    @Size(min = 3, max = 50, message = "Must be between 3 and 50 characters.")
+    @NotBlank(message = "Name is required")
     private String name;
+    @Size(max = 500, message = "Description too long!")
     private String description;
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email. Try again.")
+    private String contactEmail;
     private int id;
     private static int nextId = 1;
 
 
-    public Event(String name, String description) {
+    public Event(String name, String description, String contactEmail) {
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
         this.id = nextId;
         nextId++;
     }
+    public Event() {}
 
     public String getName() {
         return name;
@@ -31,6 +43,15 @@ public class Event {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
     public int getId() {
         return id;
     }
