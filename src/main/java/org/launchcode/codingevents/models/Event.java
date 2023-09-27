9f107855b0d6af567224453aa58c2e43cc5ab177
CannibalCrab <138933689+EEmeri000@users.noreply.github.com>
2023-09-27 09:56:44 -0400

    Chap 18 all Walkthroughs complete

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -7,6 +7,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -37,6 +39,8 @@ public class Event extends AbstractEntity{
     @ManyToOne
     @NotNull(message = "Category is required")
     private EventCategory eventCategory;
+    @ManyToMany
+    private final List<Tag> tags = new ArrayList<>();
 
     public Event(String name, EventCategory eventCategory) {
         this.name = name;
@@ -90,12 +94,20 @@ public class Event extends AbstractEntity{
 //        return id;
 //    }
 
+
+
     public EventDetails getEventDetails() {
         return eventDetails;
     }
 
     public void setEventDetails(EventDetails eventDetails) {
         this.eventDetails = eventDetails;
+    }
+    public List<Tag> getTags() {
+        return tags;
+    }
+    public void addTag(Tag tag) {
+        this.tags.add(tag);
     }
 
     @Override
